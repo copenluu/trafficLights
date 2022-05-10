7da330d7a61457d9e9632c02f931dccac19c62fb
cope <ocollinscope@gmail.com>
2022-05-10 18:27:10 +0100

    started working on adding input changes

---
+++ trafficLightsMobileDev/Assets/Scripts/GetInputTest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class GetInputTest : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI S1;
+
+    
+}
